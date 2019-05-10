8e5e4de8482accf83434d135f8395a077ab20d47
Afras Ali <afras.ali21@gmail.com>
2019-05-10 15:35:56 +0530

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase";
- 
-const config = {
-  apiKey: "AIzaSyALh8pYKPh9_l5LfGOLrxcFVI-CyufmK74",
-  authDomain: "project-274733781475.firebaseapp.com",
-  databaseURL: "https://hnur1-497b5.firebaseio.com/",
-//   storageBucket: "<BUCKET>.appspot.com"
-};
-firebase.initializeApp(config);
